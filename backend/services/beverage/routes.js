b464a49c77e3cd647640a6dadf8cff22852d0ab4
Ishaan Gupta <f20230339@goa.bits-pilani.ac.in>
2025-12-27 23:38:54 +0530

    beverage and event service working

--- backend/services/beverage/routes.js
+++
@@ -1,5 +0,0 @@
-router.get("/restaurants/:id/beverages", service.getAll);
-router.post("/restaurants/:id/beverages", service.create);
-router.get("/beverages/:id", service.getOne);
-router.patch("/beverages/:id", service.update);
-router.delete("/beverages/:id", service.remove);
