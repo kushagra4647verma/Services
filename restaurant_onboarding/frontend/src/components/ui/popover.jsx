b127ac484fe5d920dc2b62b961490c7771e6420f
CORRECT_GITHUB_USERNAME <teamsipzy@protonmail.com>
2026-01-06 21:43:08 +0530

    frontend done

--- restaurant_onboarding/frontend/src/components/ui/popover.jsx
+++ restaurant_onboarding/frontend/src/components/ui/popover.jsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 const Popover = PopoverPrimitive.Root
 
 const PopoverTrigger = PopoverPrimitive.Trigger
-
+///hello
 const PopoverAnchor = PopoverPrimitive.Anchor
 
 const PopoverContent = React.forwardRef(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
